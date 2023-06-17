9b2804751213851406f0eceb3c0862e027d6adbb
monasrour <mona.omar.srour@gmail.com>
2023-06-17 09:18:32 +0300

    Adding Authentication To our Apis Using SanCtum

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\PostController;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,6 +21,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts',[PostController::class, 'index']);
-Route::get('/posts/{post}',[PostController::class, 'show']);
+Route::get('/posts',[PostController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/posts/{post}',[PostController::class, 'show'])->middleware('auth:sanctum');
 
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
